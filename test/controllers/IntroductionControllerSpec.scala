3d96418ad2f60bbcdc75c535319079a50c7c4f06
jonathan.leather <jonathan.leather@digital.hmrc.gov.uk>
2016-06-29 08:53:15 +0100

    TAVC-946 updated view and controller for start page - 100% coverage on unit tests

--- test/controllers/IntroductionControllerSpec.scala
+++ test/controllers/IntroductionControllerSpec.scala
@@ -16,173 +16,81 @@
 
 package controllers
 
-import connectors.KeystoreConnector
-import controllers.examples.DoSubmissionController
-import models.DoSubmissionModel
-import org.jsoup._
-import org.mockito.Matchers
-import org.mockito.Mockito._
-import org.scalatest.mock.MockitoSugar
+import java.util.UUID
+import org.jsoup.Jsoup
 import play.api.i18n.Messages
-import play.api.libs.json.Json
-import play.api.mvc.{AnyContentAsFormUrlEncoded, Result}
+import play.api.mvc.{AnyContent, Action}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import uk.gov.hmrc.http.cache.client.CacheMap
-import uk.gov.hmrc.play.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.play.http.SessionKeys
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
-import scala.concurrent.Future
+class IntroductionControllerSpec extends UnitSpec with WithFakeApplication {
 
-class IntroductionControllerSpec extends UnitSpec with WithFakeApplication with MockitoSugar {
+  class fakeRequestTo(url: String, controllerAction: Action[AnyContent]) {
+    val fakeRequest = FakeRequest("GET", "/investment-tax-relief/" + url)
+    val result = controllerAction(fakeRequest)
+    val jsoupDoc = Jsoup.parse(bodyOf(result))
+  }
 
-  implicit val hc = new HeaderCarrier()
+  class fakeRequestToWithSessionId(url: String, controllerAction: Action[AnyContent]) {
+    val sessionId = UUID.randomUUID.toString
+    val fakeRequest = FakeRequest("GET", "/investment-tax-relief/" + url).withSession(SessionKeys.sessionId -> s"session-$sessionId")
+    val result = controllerAction(fakeRequest)
+    val jsoupDoc = Jsoup.parse(bodyOf(result))
+  }
 
-  def setupTarget(getData: Option[DoSubmissionModel], postData: Option[DoSubmissionModel]): DoSubmissionController = {
+  class fakePostTo(url: String, controllerAction: Action[AnyContent]) {
+    val fakeRequest = FakeRequest("POST", "/investment-tax-relief/" + url)
+    val result = controllerAction(fakeRequest)
+    val jsoupDoc = Jsoup.parse(bodyOf(result))
+  }
 
-    val mockKeystoreConnector = mock[KeystoreConnector]
 
 
-    when(mockKeystoreConnector.fetchAndGetFormData[DoSubmissionModel](Matchers.anyString())(Matchers.any(), Matchers.any()))
-      .thenReturn(Future.successful(getData))
+  "IntroductionController.show" should {
 
-    lazy val data = CacheMap("form-id", Map("data" -> Json.toJson(postData.getOrElse(DoSubmissionModel("")))))
-    when(mockKeystoreConnector.saveFormData[DoSubmissionModel](Matchers.anyString(), Matchers.any())(Matchers.any(), Matchers.any()))
-      .thenReturn(Future.successful(data))
+    "when called with no session" should {
 
-    new DoSubmissionController {
-      override val keyStoreConnector: KeystoreConnector = mockKeystoreConnector
+      object IntroductionTestDataItem extends fakeRequestTo("", IntroductionController.show())
+
+      "return a 303" in {
+        status(IntroductionTestDataItem.result) shouldBe 303
+      }
+    }
+
+    "when called with a session" should {
+
+      object IntroductionWithSessionTestDataItem extends fakeRequestToWithSessionId("", IntroductionController.show())
+
+      "return a 200" in {
+        status(IntroductionWithSessionTestDataItem.result) shouldBe 200
+      }
     }
   }
 
-  // GET Tests
-  "Calling the DoSubmissio.disabledTrustee" when {
+  "IntroductionController.submit" should {
 
-    lazy val fakeRequest = FakeRequest("GET", "/investment-tax-relief/start").withSession(SessionKeys.sessionId -> "12345")
+    "when a submit is called" should{
 
-    "not supplied with a pre-existing stored model" should {
+      object IntroductionTestDataItem extends fakePostTo("", IntroductionController.submit())
 
-      val target = setupTarget(None, None)
-      lazy val result = target.show(fakeRequest)
-      lazy val document = Jsoup.parse(bodyOf(result))
+      "return a 303" in {
+        status(IntroductionTestDataItem.result) shouldBe 303
+      }
+    }
+  }
 
-      "return a 200" in {
-        status(result) shouldBe 200
+  "IntroductionController.restart" should {
+
+    "when restart is called" should{
+
+      object IntroductionTestDataItem extends fakePostTo("", IntroductionController.restart())
+
+      "return a 303" in {
+        status(IntroductionTestDataItem.result) shouldBe 303
       }
 
-      "return some HTML that" should {
-        "contain some text and use the character set utf-8" in {
-          contentType(result) shouldBe Some("text/html")
-          charset(result) shouldBe Some("utf-8")
-        }
-
-        "have the title 'Welcome'" in {
-          document.title shouldEqual Messages("page.start.welcome.title")
-        }
-
-        "have the correct heading" in {
-          document.body.getElementsByTag("h1").text shouldEqual Messages("page.start.welcome.heading")
-        }
-
-        //        "have a 'Back' link to ${routes.DoSubmissionController.show()}" in {
-        //          document.body.getElementById("back-link").text shouldEqual Messages("common.button.back")
-        //          document.body.getElementById("back-link").attr("href") shouldEqual routes.DateOfFirstSaleController.show.toString()
-        //        }
-
-        "have the correct text in description-one" in {
-          document.body.getElementById("description-one").parent.text shouldEqual Messages("page.start.welcome.description.one")
-        }
-
-        "have the correct text in description-two" in {
-          document.body.getElementById("description-two").parent.text shouldEqual Messages("page.start.welcome.description.two")
-        }
-
-        "have the correct text for bullet point 1-learn" in {
-          document.body.getElementById("learn").parent.text shouldEqual Messages("page.start.welcome.learn")
-        }
-
-        "have the correct text for bullet point 2-eligible" in {
-          document.body.getElementById("eligible").parent.text shouldEqual Messages("page.start.welcome.bullet.eligible")
-        }
-
-        "have the correct text for bullet point 3-certify" in {
-          document.body.getElementById("certify").parent.text shouldEqual Messages("page.start.welcome.bullet.certify")
-        }
-
-        "have the correct text for bullet point 4-claim" in {
-          document.body.getElementById("claim").parent.text shouldEqual Messages("page.start.welcome.bullet.claim")
-        }
-        //        "display a radio button with the option 'Yes'" in {
-        //          document.body.getElementById("doSubmission-yes").parent.text shouldEqual Messages("common.base.yes")
-        //        }
-        //        "display a radio button with the option 'No'" in {
-        //          document.body.getElementById("doSubmission-no").parent.text shouldEqual Messages("common.base.no")
-        //        }
-
-        "display a 'Start now' button " in {
-          document.body.getElementById("next").text shouldEqual Messages("common.button.start")
-        }
-
-        "have the correct sub heading" in {
-          document.body.getElementsByTag("h2").text shouldEqual Messages("common.readMore")
-        }
-
-        "have the correct sub heading" in {
-          document.body.getElementsByTag("h2").text shouldEqual Messages("common.readMore")
-        }
-
-        "have the correct sub heading" in {
-          document.body.attr("href") shouldEqual Messages("How to apply")
-        }
-      }
-
-      // POST Tests
-      "In DoSubmissionContoller calling the .submit action" when {
-
-        def buildRequest(body: (String, String)*): FakeRequest[AnyContentAsFormUrlEncoded] = FakeRequest("POST",
-          "/investment-tax-relief/examples/examples-do-submission")
-          .withSession(SessionKeys.sessionId -> "12345")
-          .withFormUrlEncodedBody(body: _*)
-
-        def executeTargetWithMockData(data: String): Future[Result] = {
-          lazy val fakeRequest = buildRequest(("doSubmission", data))
-          val mockData = new DoSubmissionModel(data)
-          val target = setupTarget(None, Some(mockData))
-          target.submit(fakeRequest)
-        }
-
-        "submitting a valid form with 'Yes'" should {
-
-          lazy val result = executeTargetWithMockData("Yes")
-
-          "return a 303" in {
-            status(result) shouldBe 303
-          }
-        }
-
-        "submitting a valid form with 'No'" should {
-
-          lazy val result = executeTargetWithMockData("No")
-
-          "return a 303" in {
-            status(result) shouldBe 303
-          }
-        }
-
-        "submitting an invalid form with no content" should {
-
-          lazy val result = executeTargetWithMockData("")
-          lazy val document = Jsoup.parse(bodyOf(result))
-
-          "return a 400" in {
-            status(result) shouldBe 400
-          }
-
-          "display a visible Error Summary field" in {
-            document.getElementById("error-summary-display").hasClass("error-summary--show")
-          }
-        }
-      }
     }
   }
 }
