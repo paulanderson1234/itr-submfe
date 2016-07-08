4882d96b3c4d31f3f8ed3114e0e6506a014e5fb1
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2016-07-08 17:11:49 +0100

    TAVC-1038

---
+++ app/controllers/PercentageStaffWithMastersController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import common.KeystoreKeys
+import connectors.KeystoreConnector
+import controllers.predicates.ValidActiveSession
+import forms.PercentageStaffWithMastersForm._
+import models.PercentageStaffWithMastersModel
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc._
+import scala.concurrent.Future
+import views.html._
+
+object PercentageStaffWithMastersController extends PercentageStaffWithMastersController{
+  val keyStoreConnector: KeystoreConnector = KeystoreConnector
+}
+
+trait PercentageStaffWithMastersController extends FrontendController with ValidActiveSession {
+
+  val keyStoreConnector: KeystoreConnector
+
+  val show = ValidateSession.async { implicit request =>
+    keyStoreConnector.fetchAndGetFormData[PercentageStaffWithMastersModel](KeystoreKeys.percentageStaffWithMasters).map {
+      case Some(data) => Ok(knowledgeIntensive.PercentageStaffWithMasters(percentageStaffWithMastersForm.fill(data)))
+      case None => Ok(knowledgeIntensive.PercentageStaffWithMasters(percentageStaffWithMastersForm))
+    }
+  }
+
+  val submit = Action.async { implicit request =>
+    val response = percentageStaffWithMastersForm.bindFromRequest().fold(
+      formWithErrors => {
+        BadRequest(knowledgeIntensive.PercentageStaffWithMasters(formWithErrors))
+      },
+      validFormData => {
+        keyStoreConnector.saveFormData(KeystoreKeys.percentageStaffWithMasters, validFormData)
+        Redirect(routes.PercentageStaffWithMastersController.show())
+      }
+    )
+    Future.successful(response)
+  }
+}
---
+++ test/forms/PercentageStaffWithMastersFormSpec.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import models.{IsKnowledgeIntensiveModel, PercentageStaffWithMastersModel}
+import play.api.data.FormError
+import play.api.i18n.Messages
+import play.api.libs.json.Json
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import play.api.test.FakeRequest
+import uk.gov.hmrc.play.test.UnitSpec
+
+class PercentageStaffWithMastersFormSpec extends UnitSpec {
+
+  private def bindSuccess(request: FakeRequest[AnyContentAsFormUrlEncoded]) = {
+    PercentageStaffWithMastersForm.percentageStaffWithMastersForm.bindFromRequest()(request).fold(
+      formWithErrors => None,
+      userData => Some(userData)
+    )
+  }
+
+  private def bindWithError(request: FakeRequest[AnyContentAsFormUrlEncoded]): Option[FormError] = {
+    PercentageStaffWithMastersForm.percentageStaffWithMastersForm.bindFromRequest()(request).fold(
+      formWithErrors => Some(formWithErrors.errors(0)),
+      userData => None
+    )
+  }
+
+  val percentageStaffWithMastersJson = """{"staffWithMasters":"Yes"}"""
+  val percentageStaffWithMastersModel = PercentageStaffWithMastersModel("Yes")
+
+  "The Percentage Staff With Masters Form" should {
+    "Return an error if no radio button is selected" in {
+      val request = FakeRequest("GET", "/").withFormUrlEncodedBody(
+        "staffWithMasters" -> ""
+      )
+      bindWithError(request) match {
+        case Some(err) => {
+          err.key shouldBe "staffWithMasters"
+          err.message shouldBe Messages("error.required")
+          err.args shouldBe Array()
+        }
+        case _ => {
+          fail("Missing error")
+        }
+      }
+    }
+  }
+
+  "The Percentage Staff With Masters Form" should {
+    "not return an error if the 'Yes' option is selected" in {
+      val request = FakeRequest("GET", "/").withFormUrlEncodedBody(
+        "staffWithMasters" -> "Yes"
+      )
+      bindWithError(request) match {
+        case Some(err) => {
+          fail("Validation error not expected")
+        }
+        case _ => ()
+      }
+    }
+  }
+
+
+  "The Percentage Staff With Masters Form" should {
+    "not return an error if the 'No' option is selected" in {
+      val request = FakeRequest("GET", "/").withFormUrlEncodedBody(
+        "staffWithMasters" -> "No"
+      )
+      bindWithError(request) match {
+        case Some(err) => {
+          fail("Validation error not expected")
+        }
+        case _ => ()
+      }
+    }
+  }
+
+  // model to json
+  "The Percentage Staff With Masters Form model" should {
+    "load convert to JSON successfully" in {
+
+      implicit val formats = Json.format[PercentageStaffWithMastersModel]
+
+      val percentageStaffWithMasters = Json.toJson(percentageStaffWithMastersModel).toString()
+      percentageStaffWithMasters shouldBe percentageStaffWithMastersJson
+
+    }
+  }
+
+  // form model to json - apply
+  "The Percentage Staff With Masters Form model" should {
+    "call apply correctly on the model" in {
+      implicit val formats = Json.format[PercentageStaffWithMastersModel]
+      val percentageStaffWithMastersForm = PercentageStaffWithMastersForm.percentageStaffWithMastersForm.fill(percentageStaffWithMastersModel)
+      percentageStaffWithMastersForm.get.staffWithMasters shouldBe "Yes"
+    }
+
+    // form json to model - unapply
+    "call unapply successfully to create expected Json" in {
+      implicit val formats = Json.format[PercentageStaffWithMastersModel]
+      val percentageStaffWithMastersForm = PercentageStaffWithMastersForm.percentageStaffWithMastersForm.fill(percentageStaffWithMastersModel)
+      val formJson = Json.toJson(percentageStaffWithMastersForm.get).toString()
+      formJson shouldBe percentageStaffWithMastersJson
+    }
+  }
+}
