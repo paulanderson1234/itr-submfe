9aeedbe9ff55683cc34146c74959805bd6bd1e30
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2016-07-25 19:21:05 +0100

    added preliminary conditional  back url to submit/show

--- app/controllers/NewGeographicalMarketController.scala
+++ app/controllers/NewGeographicalMarketController.scala
@@ -23,6 +23,8 @@ import forms.NewGeographicalMarketForm._
 import models.NewGeographicalMarketModel
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import play.api.mvc._
+import uk.gov.hmrc.play.http.HeaderCarrier
+
 import scala.concurrent.Future
 import views.html.investment.NewGeographicalMarket
 
@@ -35,22 +37,31 @@ trait NewGeographicalMarketController extends FrontendController with ValidActiv
   val keyStoreConnector: KeystoreConnector
 
   val show = ValidateSession.async { implicit request =>
-    keyStoreConnector.fetchAndGetFormData[NewGeographicalMarketModel](KeystoreKeys.newGeographicalMarket).map {
-      case Some(data) => Ok(NewGeographicalMarket(newGeographicalMarketForm.fill(data)))
-      case None => Ok(NewGeographicalMarket(newGeographicalMarketForm))
+    def routeRequest(backUrl: String) = {
+      keyStoreConnector.fetchAndGetFormData[NewGeographicalMarketModel](KeystoreKeys.newGeographicalMarket) map {
+        case Some(data) => Ok(NewGeographicalMarket(newGeographicalMarketForm.fill(data), backUrl))
+        case None => Ok(NewGeographicalMarket(newGeographicalMarketForm, backUrl))
+      }
     }
+
+    for {
+      link <- getBackLink
+      route <- routeRequest(link)
+    } yield route
   }
 
   val submit = Action.async { implicit request =>
-    val response = newGeographicalMarketForm.bindFromRequest().fold(
-      formWithErrors => {
-        BadRequest(NewGeographicalMarket(formWithErrors))
-      },
-      validFormData => {
-        keyStoreConnector.saveFormData(KeystoreKeys.newGeographicalMarket, validFormData)
-        Redirect(routes.NewProductController.show)
+    newGeographicalMarketForm.bindFromRequest.fold(
+      invalidForm => getBackLink.flatMap(url => Future.successful(BadRequest(NewGeographicalMarket(invalidForm, url)))),
+      validForm => {
+        keyStoreConnector.saveFormData[NewGeographicalMarketModel](KeystoreKeys.newGeographicalMarket, validForm)
+        Future.successful(Redirect(routes.NewProductController.show()))
       }
     )
-    Future.successful(response)
+  }
+
+  def getBackLink(implicit hc: HeaderCarrier): Future[String] = {
+    //TODO: this needs to reuse logic that determines the forward navigation (3 possible routes)
+    Future.successful(routes.ProposedInvestmentController.show.toString())
   }
 }
