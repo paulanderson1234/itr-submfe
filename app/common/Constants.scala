f4aa636e2664e7c8e9f2559880439b1f9d4d66fc
david.gordon <david.gordon@digital.hmrc.gov.uk>
2016-08-26 16:44:21 +0100

    TAVC-1223 Initial commit

--- app/common/Constants.scala
+++ app/common/Constants.scala
@@ -26,4 +26,6 @@ trait Constants {
   val SuggestedTextMaxLength: Int = 2048
   val IsKnowledgeIntesnsiveYears : String = "10"
   val IsNotKnowledgeIntesnsiveYears : String = "7"
+  val lifetimeLogicLimitKi : Int = 20000000
+  val lifetimeLogicLimitNotKi : Int = 12000000
 }
