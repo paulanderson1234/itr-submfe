ab012c2de210c6ee7ab72416367d6d63de990d1b
christopher-boucher <christopher.boucher@digital.hmrc.gov.uk>
2016-10-29 00:14:19 +0100

    Added sign-out page and button, only shown on pages that require the user to be logged in.

---
+++ app/controllers/SignOutController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import auth.AuthorisedAndEnrolledForTAVC
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import connectors.EnrolmentConnector
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import views.html.signout.SignedOut
+
+import scala.concurrent.Future
+
+object SignOutController extends SignOutController {
+  override lazy val enrolmentConnector = EnrolmentConnector
+  override lazy val applicationConfig = FrontendAppConfig
+  override lazy val authConnector = FrontendAuthConnector
+}
+
+trait SignOutController extends FrontendController with AuthorisedAndEnrolledForTAVC {
+
+  def signout(): Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
+    Future.successful(Redirect(s"${applicationConfig.ggSignOutUrl}?continue=${applicationConfig.signOutPageUrl}"))
+  }
+
+  def show(): Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(SignedOut()))
+  }
+
+}
