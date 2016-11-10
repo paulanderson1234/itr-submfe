890c49db23796add6993d1012349b33891e794fb
jade.young <jade.young@digital.hmrc.gov.uk>
2016-11-10 13:07:37 +0000

    TAVC-1612 create error page

---
+++ app/controllers/TradingForTooLong.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import auth.AuthorisedAndEnrolledForTAVC
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import connectors.{EnrolmentConnector, S4LConnector}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+object TradingForTooLongController extends TradingForTooLongController
+{
+  val s4lConnector: S4LConnector = S4LConnector
+  override lazy val applicationConfig = FrontendAppConfig
+  override lazy val authConnector = FrontendAuthConnector
+  override lazy val enrolmentConnector = EnrolmentConnector
+}
+
+trait TradingForTooLongController extends FrontendController with AuthorisedAndEnrolledForTAVC{
+
+  val show = AuthorisedAndEnrolled.async { implicit user => implicit request =>
+    Future.successful(Ok(views.html.investment.TradingForTooLong()))
+  }
+
+  val submit = AuthorisedAndEnrolled.async { implicit user => implicit request =>
+    Future.successful(Redirect(routes.TaxpayerReferenceController.show()))
+  }
+}
