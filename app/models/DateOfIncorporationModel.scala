18c08bcc2dc11829f57a65321aab1feca9c7b70b
christopher-boucher <christopher.boucher@digital.hmrc.gov.uk>
2017-01-03 13:28:12 +0000

    Updated copyright to 2017

--- app/models/DateOfIncorporationModel.scala
+++ app/models/DateOfIncorporationModel.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,4 +25,3 @@ object DateOfIncorporationModel extends DateFormatter{
   implicit val format = Json.format[DateOfIncorporationModel]
   implicit val writes = Json.writes[DateOfIncorporationModel]
 }
-
