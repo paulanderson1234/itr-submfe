6014baebde31f4e9dc419d9b690e608e127e10ab
david.gordon <david.gordon@digital.hmrc.gov.uk>
2017-01-17 12:18:42 +0000

    TAVC-DomainFix chnaged submissionUrl

--- app/config/frontendAppConfig.scala
+++ app/config/frontendAppConfig.scala
@@ -62,7 +62,7 @@ object FrontendAppConfig extends AppConfig with ServicesConfig {
   override lazy val reportAProblemPartialUrl = s"$contactFrontendService/problem_reports_ajax?service=$contactFormServiceIdentifier"
   override lazy val reportAProblemNonJSUrl = s"$contactFrontendService/problem_reports_nonjs?service=$contactFormServiceIdentifier"
   override lazy val submissionUrl = baseUrl("investment-tax-relief-submission")
-  override lazy val attachmentFileUploadUrl = s"$attachmentsFrontEndServiceBaseUrl/file-upload?continueUrl=$submissionFrontendServiceBaseUrl/investment-tax-relief/check-your-answers"
+  override lazy val attachmentFileUploadUrl = s"$attachmentsFrontEndServiceBaseUrl/file-upload?continueUrl=$submissionFrontendServiceBaseUrl/check-your-answers"
   override lazy val uploadFeatureEnabled: Boolean = getFeature(s"$env.features.UploadEnabled")
 
 }
