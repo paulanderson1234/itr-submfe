4e0e5515427cd2dc0215e670a671cec0d4b34827
david.gordon <david.gordon@digital.hmrc.gov.uk>
2017-02-20 13:36:56 +0000

    TAVC-LongFix changed int to long to support edge case total amounts

--- app/utils/CostFormatter.scala
+++ app/utils/CostFormatter.scala
@@ -23,12 +23,12 @@ import common.Constants
 trait CostFormatter {
 
   def getOperatingAndRDCostsAsFormattedString(value: String, taxYear: String): String= {
-    val transformedValue = Transformers.integerToFormattedNumber(value.toInt)
+    val transformedValue = Transformers.numberToFormattedNumber(value.toInt)
     Constants.taxYearFormattedAnswer(transformedValue,taxYear)
   }
 
-  def getAmountAsFormattedString(value: Int): String = {
-    val transformedValue = Transformers.integerToFormattedNumber(value)
+  def getAmountAsFormattedString(value: AnyVal): String = {
+    val transformedValue = Transformers.numberToFormattedNumber(value)
     Constants.amountFormattedAnswer(transformedValue)
   }
 }
