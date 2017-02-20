a3542b028709f8839e0cf35dc8a8318b6887e68b
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2017-02-20 17:22:45 +0000

    TAVC-1978 Testendpoint for EISSEIS and fix EIS and SEIS routing

--- app/testOnly/controllers/seis/TestEndpointSEISController.scala
+++ app/testOnly/controllers/seis/TestEndpointSEISController.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package testOnly.controllers
+package testOnly.controllers.seis
 
 import auth.{AuthorisedAndEnrolledForTAVC, TAVCUser}
 import common.{Constants, KeystoreKeys}
