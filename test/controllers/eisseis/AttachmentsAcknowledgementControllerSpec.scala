70b7ff80771fdab42749c529a401951268596b6a
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2017-03-17 12:39:26 +0000

    TAVC-2186 fixing the test cases for all flows

--- test/controllers/eisseis/AttachmentsAcknowledgementControllerSpec.scala
+++ test/controllers/eisseis/AttachmentsAcknowledgementControllerSpec.scala
@@ -23,7 +23,7 @@ import config.FrontendAuthConnector
 import connectors.{EnrolmentConnector, S4LConnector, SubmissionConnector}
 import controllers.helpers.BaseSpec
 import models.submission.{SchemeTypesModel, SubmissionResponse}
-import models.{ContactDetailsModel, SubmissionRequest, YourCompanyNeedModel}
+import models.{ContactDetailsModel, SubmissionRequest, TradeStartDateModel, YourCompanyNeedModel}
 import org.mockito.Matchers
 import org.mockito.Mockito._
 import play.api.libs.json.Json
@@ -56,6 +56,8 @@ class AttachmentsAcknowledgementControllerSpec extends BaseSpec {
   class SetupPageFull() {
     setUpMocks(mockS4lConnector)
     setUpMocksRegistrationService(mockRegistrationDetailsService)
+    when(mockS4lConnector.fetchAndGetFormData[TradeStartDateModel](Matchers.eq(KeystoreKeys.tradeStartDate))
+      (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Some(tradeStartDateModelYes))
   }
 
   class SetupPageMinimum() {
@@ -71,6 +73,8 @@ class AttachmentsAcknowledgementControllerSpec extends BaseSpec {
       .thenReturn(Future.successful(HttpResponse(OK, Some(Json.toJson(submissionResponse)))))
     when(mockS4lConnector.fetchAndGetFormData[SchemeTypesModel](Matchers.eq(KeystoreKeys.selectedSchemes))
       (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Some(schemeTypesEIS))
+    when(mockS4lConnector.fetchAndGetFormData[TradeStartDateModel](Matchers.eq(KeystoreKeys.tradeStartDate))
+      (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Some(tradeStartDateModelYes))
   }
 
   "AcknowledgementController" should {
@@ -91,18 +95,18 @@ class AttachmentsAcknowledgementControllerSpec extends BaseSpec {
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 200 and delete the current application when a valid submission data is submitted" in new SetupPageFull {
       when(mockFileUploadService.getUploadFeatureEnabled).thenReturn(false)
       when(mockS4lConnector.clearCache()(Matchers.any(),Matchers.any())).thenReturn(HttpResponse(NO_CONTENT))
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe OK
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 200, close the file upload envelope and " +
       "delete the current application when a valid submission data is submitted with the file upload flag enabled" in new SetupPageFull {
       when(mockFileUploadService.getUploadFeatureEnabled).thenReturn(true)
@@ -111,162 +115,162 @@ class AttachmentsAcknowledgementControllerSpec extends BaseSpec {
         (Matchers.any(), Matchers.any(),Matchers.any())).thenReturn(Future.successful(envelopeId))
       when(mockS4lConnector.clearCache()(Matchers.any(),Matchers.any())).thenReturn(HttpResponse(NO_CONTENT))
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe OK
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 200 and delete the current application when a valid submission data is submitted with minimum expected data" in new SetupPageMinimum {
       when(mockFileUploadService.getUploadFeatureEnabled).thenReturn(false)
       when(mockS4lConnector.clearCache()(Matchers.any(),Matchers.any())).thenReturn(HttpResponse(NO_CONTENT))
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe OK
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory KiProcessingModel is missing from keystore" in {
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, kiModel = None,
         Some(natureOfBusinessValid), Some(contactValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory NatureOfBusinessModel is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         natureBusiness = None, Some(contactValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory ContactDetailsModel is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), contactDetails = None, Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory ProposedInvestmentModel is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), Some(contactValid), proposedInvestment = None,
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory InvestmentGrowModel is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), Some(contactValid), Some(proposedInvestmentValid),
         investGrow = None, Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory DateOfIncorporationModel is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), Some(contactValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), dateIncorp = None, Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory AddressModel (contact address) is missing from keystore" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), Some(contactValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), contactAddress = None, true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 303 redirect if mandatory registrationDetailsModel is from registration details service" in {
 
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValid),
         Some(natureOfBusinessValid), Some(contactValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), false)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe SEE_OTHER
       redirectLocation(result) shouldBe Some(controllers.routes.ApplicationHubController.show().url)
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 200 if KI is set to false" in {
       when(mockS4lConnector.clearCache()(Matchers.any(),Matchers.any())).thenReturn(HttpResponse(NO_CONTENT))
       setUpMocksTestMinimumRequiredModels(mockS4lConnector, mockRegistrationDetailsService, Some(kiProcModelValidAssertNo),
         Some(natureOfBusinessValid), Some(contactDetailsValid), Some(proposedInvestmentValid),
         Some(investmentGrowValid), Some(dateOfIncorporationValid), Some(fullCorrespondenceAddress), true)
       setupMocks()
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe OK
     }
   }
 
-  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController" should {
+  "Sending an Authenticated and Enrolled GET request with a session to AttachmentsAcknowledgementController for combined" should {
     "return a 5xx when an invalid email is submitted" in new SetupPageFull {
       when(mockS4lConnector.fetchAndGetFormData[SchemeTypesModel](Matchers.eq(KeystoreKeys.selectedSchemes))
         (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Some(schemeTypesEIS))
       when(mockSubmissionConnector.submitAdvancedAssurance(Matchers.any(), Matchers.any())(Matchers.any()))
         .thenReturn(Future.successful(HttpResponse(INTERNAL_SERVER_ERROR)))
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       val result = TestController.show.apply(authorisedFakeRequest)
       status(result) shouldBe INTERNAL_SERVER_ERROR
     }
   }
 
-  "Sending a POST request to the Acknowledgement controller when authenticated and enrolled" should {
+  "Sending a POST request to the Attachments Acknowledgement Controller for combined when authenticated and enrolled" should {
     "redirect to the feedback page" in {
-      mockEnrolledRequest(eisSchemeTypesModel)
+      mockEnrolledRequest(eisSeisSchemeTypesModel)
       submitWithSessionAndAuth(TestController.submit)(
         result => {
           status(result) shouldBe SEE_OTHER
