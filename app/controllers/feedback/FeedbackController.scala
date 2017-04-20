95cd812be07e107dcfea8e000a91fbdefa1899b5
david.gordon <david.gordon@digital.hmrc.gov.uk>
2017-04-20 12:50:56 +0100

    TAVC-FeedbackFix changed feedback config urls

--- app/controllers/feedback/FeedbackController.scala
+++ app/controllers/feedback/FeedbackController.scala
@@ -77,12 +77,12 @@ trait FeedbackController extends FrontendController with AuthorisedAndEnrolledFo
 
   private val TICKET_ID = "ticketId"
   private def feedbackFormPartialUrl(implicit request: Request[AnyContent]) =
-    s"${applicationConfig.contactFrontendService}/beta-feedback/form/?submitUrl=${urlEncode(localSubmitUrl)}" +
+    s"${applicationConfig.contactFrontendPartialBaseUrl}/contact/beta-feedback/form/?submitUrl=${urlEncode(localSubmitUrl)}" +
       s"&service=${urlEncode(applicationConfig.contactFormServiceIdentifier)}&referer=${urlEncode(contactFormReferer)}"
   private def feedbackHmrcSubmitPartialUrl(implicit request: Request[AnyContent]) =
-    s"${applicationConfig.contactFrontendService}/beta-feedback/form?resubmitUrl=${urlEncode(localSubmitUrl)}"
+    s"${applicationConfig.contactFrontendPartialBaseUrl}/contact/beta-feedback/form?resubmitUrl=${urlEncode(localSubmitUrl)}"
   private def feedbackThankYouPartialUrl(ticketId: String)(implicit request: Request[AnyContent]) =
-    s"${applicationConfig.contactFrontendService}/beta-feedback/form/confirmation?ticketId=${urlEncode(ticketId)}"
+    s"${applicationConfig.contactFrontendPartialBaseUrl}/contact/beta-feedback/form/confirmation?ticketId=${urlEncode(ticketId)}"
 
   def show: Action[AnyContent] = AuthorisedAndEnrolled.async {
     implicit user => implicit request =>
