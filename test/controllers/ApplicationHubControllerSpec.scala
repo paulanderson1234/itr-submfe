a744a4eebcd86e21ebdcfa273f984c88c0537440
David Gordon <david.gordon@digital.hmrc.gov.uk>
2017-11-03 11:23:11 +0000

    TAVC-4211 Linked DeletCSApplication page to AA hub

--- test/controllers/ApplicationHubControllerSpec.scala
+++ test/controllers/ApplicationHubControllerSpec.scala
@@ -339,12 +339,23 @@ class ApplicationHubControllerSpec extends BaseSpec{
 
     "Sending a POST request to ApplicationHubController delete method when authenticated and enrolled" should {
       "redirect to the delete confirmation controller" in {
-        when(mockS4lConnector.clearCache()(Matchers.any(), Matchers.any())).thenReturn(HttpResponse(NO_CONTENT))
         mockEnrolledRequest()
         submitWithSessionAndAuth(TestControllerCombined.delete)(
           result => {
             status(result) shouldBe SEE_OTHER
             redirectLocation(result) shouldBe Some(routes.ConfirmDeleteApplicationController.show().url)
+          }
+        )
+      }
+    }
+
+    "Sending a POST request to ApplicationHubController compliance statement delete method when authenticated and enrolled" should {
+      "redirect to the delete compliance statement confirmation controller" in {
+        mockEnrolledRequest()
+        submitWithSessionAndAuth(TestControllerCombined.deleteComplianceStatement)(
+          result => {
+            status(result) shouldBe SEE_OTHER
+            redirectLocation(result) shouldBe Some(routes.ConfirmDeleteCSApplicationController.show().url)
           }
         )
       }
