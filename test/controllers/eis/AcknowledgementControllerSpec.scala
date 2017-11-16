b835819ceb0a54eb85289bb54b9f259b4a6668ad
robert.webb <robert.webb@digital.hmrc.gov.uk>
2017-11-16 11:29:09 +0000

    added links into unit tests + minor conf changes

--- test/controllers/eis/AcknowledgementControllerSpec.scala
+++ test/controllers/eis/AcknowledgementControllerSpec.scala
@@ -273,7 +273,7 @@ class AcknowledgementControllerSpec extends BaseSpec {
       submitWithSessionAndAuth(TestController.submit)(
         result => {
           status(result) shouldBe SEE_OTHER
-          redirectLocation(result) shouldBe Some(controllers.feedback.routes.FeedbackController.show().url)
+          redirectLocation(result) shouldBe Some(config.FrontendAppConfig.feedbackUrl)
         }
       )
     }
